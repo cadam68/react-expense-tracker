e3f4893f073ba2c5a8b7d67c049acdf9e8e5b86e
Cyril Adam <cyril_adam@yahoo.com>
2024-04-26 19:30:03 +0200

    add spinner, floating navigation links, lazy optimisation of components and toast feature

--- src/Settings.js
+++ src/Settings.js
@@ -3,7 +3,7 @@ import { generateColorPalette } from "./services/Helper";
 export const settings = {
   maxCategories: 6,
   amountHigh: 30,
-  version: 2.1,
+  version: 2.2,
   palette: generateColorPalette(6),
   passphrase: process.env.REACT_APP_PASSPHRASE || "passphrase",
   apiKey: process.env.REACT_APP_API_KEY || "apiKey",
